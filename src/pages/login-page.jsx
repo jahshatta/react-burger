bdbe274082c7fb4aa5bb3269b9b640afe974a853
Vladimir Zverev <zverev@lanbilling.ru>
2023-02-11 18:56:32 +0400

    Restrict routes /login, /register, /forgot-password, /reset-password for logged users

--- src/pages/login-page.jsx
+++ src/pages/login-page.jsx
@@ -1,6 +1,13 @@
 import LoginForm from "../components/login-form/login-form";
+import { Navigate } from "react-router-dom";
+import { selectCurrentUser } from "../services/store/user/UserSlice";
+import { useSelector } from "react-redux";
 
 function LoginPage() {
+  const user = useSelector(selectCurrentUser);
+  if (user) {
+    return <Navigate to="/" replace />;
+  }
   return <LoginForm />;
 }
 
