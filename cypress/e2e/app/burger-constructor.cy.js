e029cc425f24c907c42ba83021ceda3573156aca
Vladimir Zverev <zverev@lanbilling.ru>
2023-04-03 13:02:26 +0400

    Add cypress test for constructor

---
+++ cypress/e2e/app/burger-constructor.cy.js
@@ -0,0 +1,36 @@
+import { BASE_URL } from "../../../src/services/api/api";
+
+describe("burger constructor functional tests", () => {
+  beforeEach(() => {
+    cy.intercept("POST", `${BASE_URL}/orders`).as("postOrder");
+    cy.intercept("POST", `${BASE_URL}/auth/login`).as("login");
+    cy.intercept("GET", `${BASE_URL}/ingredients`).as("getIngredients");
+
+    cy.visit("http://localhost:3000/login");
+    cy.get("[name^=email]").type("vovek.zverev@gmail.com");
+    cy.get("[name^=password]").type("111111");
+    cy.get("button").contains("Войти").click();
+    cy.wait("@login");
+  });
+
+  it("should open and close ingredients popup ", () => {
+    cy.wait("@getIngredients");
+    cy.get(".ingredient").first().click();
+    cy.get("#modal svg").click();
+  });
+
+  it("should create an order", () => {
+    cy.wait("@getIngredients");
+    cy.get(".ingredient").first().trigger("dragstart");
+    cy.get(".drop-area").trigger("drop");
+
+    cy.get(".ingredient").contains("Соус").first().trigger("dragstart");
+    cy.get(".drop-area").trigger("drop");
+    cy.get(".ingredient").contains("Мясо").first().trigger("dragstart");
+    cy.get(".drop-area").trigger("drop");
+    cy.get(".constructor-element").should("have.length", 4);
+
+    cy.get("button").contains("Оформить заказ").click().wait("@postOrder");
+    cy.get("#modal svg").click();
+  });
+});
--- src/components/app-header/menu/menu-item/menu-itemMenuItem.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import MenuItem from './menu-item'
-
-describe('<MenuItem />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<MenuItem />)
-  })
-})
